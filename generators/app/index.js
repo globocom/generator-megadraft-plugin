59b9b0cbc24435fbef64e2767401d13cd449482e
Marcelo Jorge Vieira <metal@alucinados.com>
2016-06-27 22:59:55 -0300

    App: Fixed license and copyright holder in index.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) <%= info.year %>, <%= inputs.authorName %> (<%= inputs.authorEmail || inputs.homepage %>)
+ * Copyright (c) 2016, Globo.com (https://github.com/globocom)
  *
  * License: MIT
  */
